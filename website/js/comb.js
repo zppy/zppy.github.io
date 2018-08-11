c5ed66f64f1a0a11ea5c65c6bfd3bd60f5b60875
Ron-e <mail@r0n.nl>
2018-08-11 07:40:46 +0200

    something broke?

--- website/js/comb.js
+++ website/js/comb.js
@@ -1,3 +1,3 @@
-$( ".navbar" ).load( "./incl/nav.htm" );
-$( ".info" ).load( "./incl/info.htm" );
-$( ".footer" ).load( "./incl/footer.htm" );
+$( ".navbar" ).load( "incl/nav.htm" );
+$( ".info" ).load( "incl/info.htm" );
+$( ".footer" ).load( "incl/footer.htm" );
